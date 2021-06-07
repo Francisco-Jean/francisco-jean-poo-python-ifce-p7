2aa9fc27d79b2f470c37982d6d074d7c7a87c3de
Francisco Jean <jeansousa2001@gmail.com>
2021-06-07 18:02:17 -0300

    Atividades

---
+++ E1-Avaliação-03/notafiscal.py
@@ -0,0 +1,44 @@
+"""
+    Módulo notafiscal -
+    Classe NotaFiscal - 
+        Atributos :
+            id        - informado.
+            codigo    - informado.
+            data      - informado.
+            cliente   - informado.
+            items     - informado
+            valornota - calculado. 
+"""
+import datetime
+from cliente        import Cliente
+from produto        import Produto
+from itemnotafiscal import ItemNotaFiscal
+
+
+class NotaFiscal():         
+    def __init__(self, Id, codigo, cliente):
+        self._Id = Id
+        self._codigo=codigo
+        self._cliente=cliente 
+        self._data=datetime.datetime.now()   
+        self._itens=[]
+        self._valorNota=0.0        
+        
+    def setCliente(self, cliente):
+        if isinstance(cliente, Cliente):
+            self._cliente=cliente    
+            
+    def adicionarItem(self, item): 
+        if isinstance(item, ItemNotaFiscal):
+            self._itens.append(item)
+        
+    def calcularNotaFiscal(self):
+        valor=0.0
+        for item in self._itens:
+            valor = valor + item._valorItem
+        self.valorNota=valor
+     
+    def imprimirNotaFiscal(self, nota):       
+        print('---------------------------------------------------------------------------------------------------------------')
+
+    
